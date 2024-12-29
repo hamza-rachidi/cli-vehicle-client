261603933a05c532dbc3a1fa7638d14e6a33e572
Hamza Rachidi <hamza.rachidi@ensg.eu>
2024-12-29 19:32:17 +0000

    installed all dependencies necessary for our project and configured the files to be ready for test, lint, and development

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  preset: "ts-jest",
+  testMatch: ["<rootDir>/src/**/?(*.)(spec|test).{ts,tsx}"],
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+};
+
