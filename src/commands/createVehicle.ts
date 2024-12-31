cc4242d19b6bf21a9462525ebf6d1f4331074169
Hamza Rachidi <hamza.rachidi@ensg.eu>
2024-12-31 01:04:41 +0000

    coding the first functionality of creating vehicles along with errors management in adddition to the global architecture to be adapted later for other functionalities

---
+++ src/commands/createVehicle.ts
@@ -0,0 +1,44 @@
+import { Command } from "commander";
+import fetch from "node-fetch";
+
+export default function create_Vehicle(address: string): Command {
+const createVehicle = new Command("create-vehicle");
+
+createVehicle
+  .description("Creates a new vehicle with the properties chosen by the user and displays a message to validate the creation request otherwise displays the error as thrown by the server in case of invalid options")
+  .requiredOption("-c,--shortcode <string>", "Shortcode (must be exactly 4 characters long)")
+  .requiredOption("-b,--battery <integer>", "Battery level ranging from 0 to 100 %")
+  .requiredOption("-l,--longitude <number>", "Longitude must be between -90 and 90")
+  .requiredOption("-L,--latitude <number>", "Latitude must be between -90 and 90")
+  .action(async (options) => {
+    const payload = {
+      shortcode: options.shortcode,
+      battery: parseInt(options.battery,10),
+      longitude: parseFloat(options.longitude),
+      latitude: parseFloat(options.latitude),
+    };
+
+    try {
+      const response = await fetch(`${address}/vehicles`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        console.error(
+          `Could not create the vehicle \n Error: ${responseData.error.code} - ${responseData.error.message}. Details: ${JSON.stringify(responseData.error.details)}`
+        );
+        process.exit(1);
+      }
+
+      console.log(`Created vehicle '${responseData.shortcode}', with ID '${responseData.id}'`);
+    } catch (error) {
+      console.error("Error connecting to the server:", error.message);
+      process.exit(1);
+    }
+  });
+  return createVehicle;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+import { Command } from "commander";
+import create_Vehicle from "./commands/createVehicle";
+
+const program = new Command();
+let address = "";
+
+program
+  .name("vehicle-cli")
+  .description("CLI designed to help the client manage vehicles through the server")
+  .version("1.0.0")
+  .option('-a, --address <url>', 'specify the server address to use')
+  .hook("preAction", (thisCommand) => {
+    // Hook pour capturer l'option API URL avant toute commande
+    address = thisCommand.opts().address;
+    if (!address) {
+      console.error("Error: --address is required");
+      process.exit(1);
+    }
+  });
+
+
+program.addCommand(create_Vehicle(address));
+// here add later : program.addCommand(listVehicle);
+program.parse(process.argv);
+
+
